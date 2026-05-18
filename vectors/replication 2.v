d35f4ca78da33d0897db7359b60cb5f1dc0ebbcf
Kalaimagal S <kalaimagals3126@gmail.com>
2026-05-18 10:55:16 +0530

    added replication 2.v

---
+++ vectors/replication 2.v
@@ -0,0 +1,7 @@
+module top_module (
+    input a, b, c, d, e,
+    output [24:0] out );
+    wire [24:0] vec1 = { {5{a}} ,  {5{b}} ,  {5{c}} ,  {5{d}} ,  {5{e}} };
+    wire [24:0] vec2 = { 5{a,b,c,d,e}};
+    assign out = ~(vec1 ^ vec2);
+endmodule
